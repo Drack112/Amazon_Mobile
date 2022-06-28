40c25736f45738dede60de2a44fb601cb38f3328
Drack112 <onepessoa3@gmail.com>
2022-06-28 18:42:03 -0300

    👌 IMPROVE: Layout using the constants

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,18 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:amazon_flutter/constants/global_variables.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Amazon Mobile",
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const Scaffold(
-        body: Center(
-          child: Text("Flutter Demo App"),
-        ),
-      ),
+      theme: ThemeData(
+          scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+          colorScheme:
+              const ColorScheme.light(primary: GlobalVariables.secondaryColor),
+          appBarTheme: const AppBarTheme(
+              elevation: 0,
+              iconTheme: IconThemeData(
+                color: Colors.black,
+              ))),
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Center(
+              child: Text("Hello Flutter"),
+            ),
+          ),
+          body: Column(
+            children: [
+              const Center(child: Text("Flutter Demo App")),
+              ElevatedButton(onPressed: () {}, child: const Text("Click"))
+            ],
+          )),
     );
   }
 }
