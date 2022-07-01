8b98349f176d207e42d5ca13bdbf0f9c8c8ece58
Drack112 <onepessoa3@gmail.com>
2022-06-30 23:31:56 -0300

    Fix Routers

--- mobile/lib/router.dart
+++ mobile/lib/router.dart
@@ -1,4 +1,5 @@
 import 'package:amazon_flutter/common/widgets/bottom_bar.dart';
+import 'package:amazon_flutter/features/screens/add_product_screen.dart';
 import 'package:amazon_flutter/features/screens/auth_screen.dart';
 import 'package:amazon_flutter/features/screens/home_screen.dart';
 import 'package:flutter/material.dart';
@@ -14,6 +15,9 @@ Route<dynamic> generateRoute(RouteSettings routeSettings) {
     case BottomBar.routeName:
       return MaterialPageRoute(
           settings: routeSettings, builder: (_) => const BottomBar());
+    case AddProductScreen.routeName:
+      return MaterialPageRoute(
+          builder: (_) => const AddProductScreen(), settings: routeSettings);
     default:
       return MaterialPageRoute(
         settings: routeSettings,
